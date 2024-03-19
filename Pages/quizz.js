66cb12c9e45b9e52b1f32837d91c9c5992e7b022
magicbob31 <guibe310@gmail.com>
2024-03-19 16:14:32 +0100

    ajout quizz

---
+++ Pages/quizz.js
@@ -0,0 +1,75 @@
+const quizz = document.querySelector("main");
+
+
+
+const questions = [{ question1: "", picture: "" },
+{
+    question2: "", picture: ""
+}]
+
+// à remplire des questions et des images corespondantes 
+
+const correctAnswer = ["", "", ""]
+
+// à remplire des reponses 
+
+const radioButton = document.querySelector("input.type")
+
+
+
+function click() {
+    if (addeventlistener.radioButton === correctAnswer) {
+        createquizz
+        // aller à la prochaine question 
+    } else {
+        // prison 
+    }
+}
+
+//fonction de validation ou non de la reponse 
+
+function createquizz() {
+    const quizz2 = document.createElement("main")
+    questions.classList.add("main")
+    main.appendchild(quizz2)
+}
+
+// fonction de creation de nouveau quizz 
+
+
+// for (let i = 0; i < correctAnswer.length; i++ ) {
+
+//     if () 
+
+// }
+
+// autre approche 
+
+const question = [{
+    question1: "",
+    choix: "",
+    picture: "",
+    reponseCorrect: "",
+},
+{
+    question1: "",
+    choix: "",
+    picture: "",
+    reponseCorrect: "",
+}]
+
+let questionCourante = 0
+
+let score = 0
+
+function chek() {
+    if (question === reponseCorrect) {
+
+        // nouveau quizz
+
+    }
+    else {
+
+        // prison
+    }
+}
