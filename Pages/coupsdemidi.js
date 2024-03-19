75e1bc363ac3e900066d67cc83d0be58ff8caf43
ileanaspl <ileana.saintpaul@gmail.com>
2024-03-19 17:32:47 +0100

    modify coupsdemidi

--- Pages/coupsdemidi.js
+++ Pages/coupsdemidi.js
@@ -2,6 +2,7 @@ import { createElementWithAttribute, appendOrPrepend, raz } from "../Services/ut
 import { devinette } from "./devinettes.js";
 
 export function coupsdemidi() {
+
   const title = createElementWithAttribute("h2", { id: "title" });
   title.innerText = "Les 31 coups de midi";
   appendOrPrepend("append", ".dynamique-content", title);
@@ -13,4 +14,26 @@ export function coupsdemidi() {
     devinette();
   });
   appendOrPrepend("append", ".dynamique-content", nextButton);
+
+const dynamiqueContent = document.querySelector(".dynamique-content");
+const gridContainer = document.createElement("div");
+gridContainer.classList.add("grid-container");
+dynamiqueContent.appendChild(gridContainer);
+
+let i;
+for (i=0; i<12; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  gridContainer.appendChild(square);
+}
+
+  square.setAttribute("class", "square-visible");
+
+  setInterval( () => {
+      for (i=0; i<0; i++) {
+      square.classList.add("square-invisible");
+      i++
+      }
+
+  }, 1000);
 }
