d9dc2312e21576bd1f44335f3e906f9206c35b9d
Modrum <tristanlaroye@hotmail.com>
2024-03-20 19:57:56 +0100

    finalisation mini game Tristan Ajout d'une classe "element disabled" dans style

--- main.js
+++ main.js
@@ -2,7 +2,22 @@ import { raz } from "./Services/util.js";
 import { home } from "./Pages/home.js";
 import { hud } from "./Pages/hud.js";
 
-export const playerInfos = {};
+export const playerInfos = { playerName: "", score: 0, totalTime: 0 };
+
+export const questions = [
+  {
+    level: "normal",
+    devinettesQuestion:
+      "Première question : Combien d'habitants peuplent l'agglomération toulousaine (à 100 000 près ?)",
+    devinettesReponse: 1063235,
+  },
+  {
+    level: "hardcore",
+    devinettesQuestion:
+      "Seconde question : Quel animal est représenté sur le blazon de la ville de Toulouse ?",
+    devinettesReponse: "agneau",
+  },
+];
 
 hud();
 raz();
