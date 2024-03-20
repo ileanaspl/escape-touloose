29214bde9659980b0b020b66836998ee3edb80de
JeromeBslz <jerome933@outlook.com>
2024-03-20 14:48:56 +0100

    premier commit jerome

--- Pages/home.js
+++ Pages/home.js
@@ -3,24 +3,33 @@ import { quizz } from "./quizz.js";
 import { playerInfos } from "../main.js";
 
 export function home() {
+  const header = document.querySelector("header");
+        header.setAttribute("class", "header-invisible");
+
+
   const title = createElementWithAttribute("h2", { id: "title" });
-  title.innerText = "Home page";
-  appendOrPrepend("append", ".dynamique-content", title);
+  title.innerText = "Escape Touloose";
+  appendOrPrepend("prepend", ".dynamique-content", title);
 
   const form = createElementWithAttribute("form", {});
-  appendOrPrepend("prepend", ".dynamique-content", form);
+  appendOrPrepend("append", ".dynamique-content", form);
 
   const labelPlayerName = createElementWithAttribute("label", { for: "player-name" });
-  labelPlayerName.innerText = "Entrer votre pseudo";
-  appendOrPrepend("prepend", "form", labelPlayerName);
+  labelPlayerName.innerText = "Entrer votre nom";
+  appendOrPrepend("append", "form", labelPlayerName);
 
   const playerName = createElementWithAttribute("input", { id: "player-name", type: "text" });
   appendOrPrepend("append", "form", playerName);
 
-  const header = document.querySelector("header");
+  const footer = document.querySelector("footer");
+        footer.setAttribute("class", "footer-invisible");
 
-  const nextButton = createElementWithAttribute("button", { id: "next-button" });
-  nextButton.innerText = "Bouton suivant";
+
+        
+
+  const nextButton = createElementWithAttribute("button", { id: "next-button",class:"valid-button" });
+
+  nextButton.innerText = "C'est parti!";
   nextButton.addEventListener("click", () => {
     // header.innerText += playerName.value;
     // playerInfos.playerName = playerName.value;
