cf8bc3354213dfa7cf9b14702692120dce4aa4cb
Modrum <tristanlaroye@hotmail.com>
2024-03-23 08:29:22 +0100

    Correction bug afficahge du footer

--- Pages/home.js
+++ Pages/home.js
@@ -4,76 +4,80 @@ import { playerInfos } from "../main.js";
 import { welcomeInTheNeighborhood } from "./intersection.js";
 
 export function home() {
+  document.body.style.backgroundImage = "url(Assets/city-toulouse.jpg)";
 
-    document.body.style.backgroundImage = "url(Assets/city-toulouse.jpg)"
+  const dynamicContent = document.querySelector(".dynamic-content");
 
-    const dynamicContent = document.querySelector(".dynamic-content");
+  const headerHome = document.querySelector("header");
+  headerHome.setAttribute("class", "header-invisible");
 
-    const headerHome = document.querySelector("header");
-    headerHome.setAttribute("class", "header-invisible");
+  const title = createElementWithAttribute("h2", { class: "title" });
+  title.innerText = "Escape Touloose";
+  appendOrPrepend("prepend", ".dynamic-content", title);
 
-    const title = createElementWithAttribute("h2", { class: "title" });
-    title.innerText = "Escape Touloose";
-    appendOrPrepend("prepend", ".dynamic-content", title);
+  const homeContainer = document.createElement("div");
+  homeContainer.classList.add("home-container");
+  dynamicContent.appendChild(homeContainer);
 
-    const homeContainer = document.createElement("div");
-    homeContainer.classList.add("home-container");
-    dynamicContent.appendChild(homeContainer);
+  const rdj = createElementWithAttribute("h2", { id: "rdj" });
+  rdj.innerText = "Règles du jeu";
+  homeContainer.appendChild(rdj);
 
-    const rdj = createElementWithAttribute("h2", { id: "rdj" });
-    rdj.innerText = "Règles du jeu";
-    homeContainer.appendChild(rdj);
+  const paragraphrdj = createElementWithAttribute("p", { id: "paragraphrdj" });
+  paragraphrdj.innerText =
+    "Vous voici sur un escape game sur le thème de la ville de Toulouse, le score dépend de plusieurs critères : le nombre de bonnes réponses, la rapidité des réponses. Attention aux mauvaises réponses. Bon courage ! ";
+  homeContainer.appendChild(paragraphrdj);
 
-    const paragraphrdj = createElementWithAttribute("p", { id: "paragraphrdj" });
-    paragraphrdj.innerText = "Vous voici sur un escape game sur le thème de la ville de Toulouse, le score dépend de plusieurs critères : le nombre de bonnes réponses, la rapidité des réponses. Attention aux mauvaises réponses. Bon courage ! ";
-    homeContainer.appendChild(paragraphrdj);
+  const myleneImage = document.createElement("img");
+  myleneImage.src = "./Assets/mylene-modified.png";
+  myleneImage.alt = "Mylène Farmer";
+  myleneImage.id = "mylene-avatar";
+  dynamicContent.appendChild(myleneImage);
 
-    const myleneImage = document.createElement("img");
-    myleneImage.src = "./Assets/mylene-modified.png";
-    myleneImage.alt = "Mylène Farmer";
-    myleneImage.id = "mylene-avatar";
-    dynamicContent.appendChild(myleneImage);
+  const nameInputSection = document.createElement("div");
+  nameInputSection.classList.add("name-input-section");
+  dynamicContent.appendChild(nameInputSection);
 
-    const nameInputSection = document.createElement("div");
-    nameInputSection.classList.add("name-input-section");
-    dynamicContent.appendChild(nameInputSection);
+  const nameLabel = createElementWithAttribute("label", { id: "name-input" });
+  nameLabel.innerText = "Entrez votre nom : ";
+  nameInputSection.appendChild(nameLabel);
 
-    const nameLabel = createElementWithAttribute("label", { id: "name-input" });
-    nameLabel.innerText = "Entrez votre nom : ";
-    nameInputSection.appendChild(nameLabel);
+  const nameInput = createElementWithAttribute("input", {
+    type: "text",
+    id: "name-input",
+    placeholder: "Votre nom",
+  });
+  nameInputSection.appendChild(nameInput);
+  dynamicContent.appendChild(nameInputSection);
 
-    const nameInput = createElementWithAttribute("input", { type: "text", id: "name-input", placeholder: "Votre nom" });
-    nameInputSection.appendChild(nameInput);
-    dynamicContent.appendChild(nameInputSection);
+  const footerHome = document.querySelector("footer");
+  console.log(footerHome);
+  footerHome.setAttribute("class", "footer-invisible");
 
-    const footerHome = document.querySelector("footer");
-    console.log(footerHome);
-    footerHome.setAttribute("class", "footer-invisible");
+  const nextButton = createElementWithAttribute("button", {
+    id: "next-button",
+    class: "valid-button",
+  });
+  nameInputSection.appendChild(nextButton);
+  nextButton.innerText = "C'est parti!";
 
-    const nextButton = createElementWithAttribute("button", { id: "next-button", class: "valid-button" });
-    nameInputSection.appendChild(nextButton);
-    nextButton.innerText = "C'est parti!";
+  const infoParagrahe = document.querySelector("#info-paragraphe");
+  const infoTitle = document.querySelector("#info-title");
+  infoTitle.innerText = "Bienvenue !";
+  infoParagrahe.innerText = "";
 
-    nextButton.addEventListener("click", () => {
-      const playerName = nextButton.previousElementSibling.value.trim();
-      console.log(nextButton.previousElementSibling.value);
-      if (playerName === "") {
-            alert("Veuillez entrer votre nom.");
-            return
-        } else {
-          const title = document.querySelector('h1')
-          headerHome.classList.remove("header-invisible");  
-          title.innerText += ` ${playerName}`;
-
-          raz();
-          welcomeInTheNeighborhood("dans le quartier de Saint-Etienne", quizz, 0);
-          footerHome.classList.remove("footer-invisible");
-        }
-    });
+  nextButton.addEventListener("click", () => {
+    const playerName = nextButton.previousElementSibling.value.trim();
+    if (playerName === "") {
+      alert("Veuillez entrer votre nom.");
+      return;
+    } else {
+      const title = document.querySelector("h1");
+      headerHome.classList.remove("header-invisible");
+      footerHome.classList.remove("footer-invisible");
+      title.innerText += ` ${playerName}`;
+      raz();
+      welcomeInTheNeighborhood("dans le quartier de Saint-Etienne", quizz, 0);
+    }
+  });
 }
-
-
-
-
-
-
